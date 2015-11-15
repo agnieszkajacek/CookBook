0afbbf05fcd853ec807591a1bfec6b50db107ab0
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2015-11-15 13:56:19 +0100

    allow remote Ajax requests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,11 @@
 class CategoriesController < ApplicationController
+  before_filter :add_allow_credentials_headers
+
+  def add_allow_credentials_headers                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    response.headers['Access-Control-Allow-Origin'] = '*'                                                                                                                                                                                                     
+    response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+  end 
+
 	def show
 		@category=Category.find(params[:id])
 		@recipes=@category.recipes.page(params[:page]).per(9)
